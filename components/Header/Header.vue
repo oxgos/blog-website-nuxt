5924018a2a4e072c40689249ac7bf31c2ea1379e
gjw <122486975@qq.com>
2018-01-16 23:43:50 +0800

    完成分类页面

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -4,8 +4,8 @@
       <h2>影子是一个会撒谎的精灵，它在虚空中流浪和等待被发现之间;在存在与不存在之间....</h2>
       <nav class="nav">
           <ul>
-              <li class="active"><router-link to="/">首页</router-link></li>
-              <li v-for="(item, index) in navData" :key="index"><router-link :to="item.path">{{item.name}}</router-link></li>
+              <li :class="{'active': current === 'Index'}" @click="current = 'Index'"><router-link to="/">首页</router-link></li>
+              <li :class="{'active': current === index}" @click="current = index" v-for="(item, index) in navData" :key="index"><router-link :to="item.path">{{item.name}}</router-link></li>
           </ul>
           <div class="logo">
             <router-link to="/"></router-link>
@@ -19,7 +19,8 @@
   export default {
     data () {
       return {
-        navData: []
+        navData: [],
+        current: 'Index'
       }
     },
     created () {
--- pages/_category.vue
+++ pages/_category.vue
@@ -1,64 +1,149 @@
 <template>
-    <div class="category">
-        <NavBread>
-            <a slot="link" href="javascript: void(0);">{{ name }}</a>
-        </NavBread>
-        <div class="bloglist">
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
-            </div>
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
-            </div>
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
-            </div>
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
-            </div>
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
-            </div>
-            <div class="newblog">
-                <ul>
-                    <h3><a href="/">个人博客从简不繁</a></h3>
-                    <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
-                    <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
-                </ul>
-                <figure><img src="~assets/img/category/001.jpg"></figure>
-                <div class="dateview">2014-04-08</div>
+    <div>
+        <div class="category">
+            <NavBread>
+                <a slot="link" href="javascript: void(0);">{{ name }}</a>
+            </NavBread>
+            <div class="bloglist">
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
+                <div class="newblog">
+                    <ul>
+                        <h3><a href="/">个人博客从简不繁</a></h3>
+                        <div class="autor"><span>作者：杨青</span><span>分类：[<a href="/">日记</a>]</span><span>浏览（<a href="/">459</a>）</span><span>评论（<a href="/">30</a>）</span></div>
+                        <p>十一月中旬开始，排名突然下降了，网站“个人博客”关键词排名从第一页第二名滑落到100页以后了，个人博客这个关键词百度已经搜不到了，仅有google、搜狗... <a href="/" target="_blank" class="readmore">全文</a></p>
+                    </ul>
+                    <figure><img src="~assets/img/category/001.jpg"></figure>
+                    <div class="dateview">2014-04-08</div>
+                </div>
             </div>
         </div>
+        <aside>
+            <div class="rnav">
+                <li class="rnav1 "><a href="javascript: void(0);">日记</a></li>
+                <li class="rnav2 "><a href="javascript: void(0);">欣赏</a></li>
+                <li class="rnav3 "><a href="javascript: void(0);">程序人生</a></li>
+                <li class="rnav4 "><a href="javascript: void(0);">经典语录</a></li>
+            </div>
+            <div class="ph_news">
+                <h2>
+                    <p>点击排行</p>
+                </h2>
+                <ul class="ph_n">
+                    <li><span class="num1">1</span><a href="/">有一种思念，是淡淡的幸福,一个心情一行文字</a></li>
+                    <li><span class="num2">2</span><a href="/">励志人生-要做一个潇洒的女人</a></li>
+                    <li><span class="num3">3</span><a href="/">女孩都有浪漫的小情怀——浪漫的求婚词</a></li>
+                    <li><span>4</span><a href="/">Green绿色小清新的夏天-个人博客模板</a></li>
+                    <li><span>5</span><a href="/">女生清新个人博客网站模板</a></li>
+                    <li><span>6</span><a href="/">Wedding-婚礼主题、情人节网站模板</a></li>
+                    <li><span>7</span><a href="/">Column 三栏布局 个人网站模板</a></li>
+                    <li><span>8</span><a href="/">时间煮雨-个人网站模板</a></li>
+                    <li><span>9</span><a href="/">花气袭人是酒香—个人网站模板</a></li>
+                </ul>
+                <h2>
+                    <p>栏目推荐</p>
+                </h2>
+                <ul>
+                    <li><a href="/">有一种思念，是淡淡的幸福,一个心情一行文字</a></li>
+                    <li><a href="/">励志人生-要做一个潇洒的女人</a></li>
+                    <li><a href="/">女孩都有浪漫的小情怀——浪漫的求婚词</a></li>
+                    <li><a href="/">Green绿色小清新的夏天-个人博客模板</a></li>
+                    <li><a href="/">女生清新个人博客网站模板</a></li>
+                    <li><a href="/">Wedding-婚礼主题、情人节网站模板</a></li>
+                    <li><a href="/">Column 三栏布局 个人网站模板</a></li>
+                    <li><a href="/">时间煮雨-个人网站模板</a></li>
+                    <li><a href="/">花气袭人是酒香—个人网站模板</a></li>
+                </ul>
+                <h2>
+                    <p>最新评论</p>
+                </h2>
+                <ul class="pl_n">
+                    <dl>
+                        <dt><img src="~assets/img/category/s8.jpg"> </dt>
+                        <dt> </dt>
+                        <dd>DanceSmile
+                            <time>49分钟前</time>
+                        </dd>
+                        <dd><a href="/">文章非常详细，我很喜欢.前端的工程师很少，我记得几年前yahoo花高薪招聘前端也招不到</a></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="~assets/img/category/s7.jpg"> </dt>
+                        <dt> </dt>
+                        <dd>yisa
+                            <time>2小时前</time>
+                        </dd>
+                        <dd><a href="/">我手机里面也有这样一个号码存在</a></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="~assets/img/category/s6.jpg"> </dt>
+                        <dt> </dt>
+                        <dd>小林博客
+                            <time>8月7日</time>
+                        </dd>
+                        <dd><a href="/">博客色彩丰富，很是好看</a></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="~assets/img/category/003.jpg"> </dt>
+                        <dt> </dt>
+                        <dd>DanceSmile
+                            <time>49分钟前</time>
+                        </dd>
+                        <dd><a href="/">文章非常详细，我很喜欢.前端的工程师很少，我记得几年前yahoo花高薪招聘前端也招不到</a></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="~assets/img/category/002.jpg"> </dt>
+                        <dt> </dt>
+                        <dd>yisa
+                            <time>2小时前</time>
+                        </dd>
+                        <dd><a href="/">我手机里面也有这样一个号码存在</a></dd>
+                    </dl>
+                </ul>
+            </div>
+        </aside>
     </div>
 </template>
 
@@ -71,15 +156,15 @@
             }
         },
         async asyncData ({ params }) {
-           let { data } = await axios.get('/categories/articleList', {
-                params: {
-                    path: `/${params.category}`
-                }
-            })
-           return {
-               articles: data.result.articles,
-               name: data.result.name
-           }
+            let { data } = await axios.get('/categories/articleList', {
+                    params: {
+                        path: `/${params.category}`
+                    }
+                })
+            return {
+                articles: data.result.articles,
+                name: data.result.name
+            }
         },
         components: {
             NavBread
@@ -200,9 +285,137 @@
             .dateview {
                 position: absolute;
                 left: -125px;
-                top: -3px;
+                top: -1px;
                 z-index: 1;
                 color: #F5F5F5;
+            }
+        }
+    }
+    aside {
+        width: 27%;
+        float: right;
+        .rnav {
+            width: 120px;
+            margin: 20px auto;
+            overflow: hidden;
+            li {
+                width: 120px;
+                text-align: center;
+                display: inline-block;
+                margin-bottom: 2px;
+                height: 40px;
+                line-height: 40px;
+                a {
+                    color: #FFF;
+                    display: block;
+                    -webkit-transition: all 1s;
+                    -moz-transition: all 1s;
+                    -o-transition: all 1s;
+                    transition: all 1s;
+                    &:hover {
+                        background: rgba(255,255,255,0.2);
+                        -webkit-transform: rotate(360deg);
+                        -moz-transform: rotate(360deg);
+                        -o-transform: rotate(360deg);
+                        transform: rotate(360deg);
+                    }
+                }
+            }
+            .rnav1 {
+                background: #e41637;
+            }
+            .rnav2 {
+                background: #f16b17;
+            }
+            .rnav3 {
+                background: #0f9c7c;
+            }
+            .rnav4 {
+                background: #46ce14;
+            }
+        }
+        .ph_news {
+            text-align: left;
+            h2 {
+                font-size: 16px;
+                font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+                color: #FFF;
+                p {
+                    height: 40px;
+                    line-height: 40px;
+                    width: 100px;
+                    padding: 0 0 0 30px;
+                    border-radius: 0 20px 20px 0;
+                    text-shadow: rgba(0,0,0,.65) 1px 1px;
+                    background: #0f9c7c;
+                }
+            }
+            ul {
+                margin: 10px;
+            }
+            li {
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                width: 100%;
+                color: #cdcbcb;
+                line-height: 24px;
+                height: 24px;
+                a {
+                    color: #cdcbcb;
+                }
+            }
+            .ph_n {
+                li {
+                    span {
+                        margin-right: 10px;
+                        font-family: "微软雅黑";
+                        color: #FFF;
+                        background: #404040;
+                        padding: 0 5px;
+                        &.num1, &.num2, &.num3 {
+                            background: #f16b17;
+                        }
+                    }
+                }
+            }
+            .pl_n {
+                dl {
+                    display: block;
+                    overflow: hidden;
+                    margin-bottom: 12px;
+                    color: #cdcbcb;
+                    dt {
+                        float: left;
+                        img {
+                            width: 50px;
+                            height: 50px;
+                            margin-right: 10px;
+                            border-radius: 50%;
+                            transition: all 1s;
+                            &:hover {
+                                -webkit-transform: rotate(360deg);
+                                -ms-transform: rotate(30deg);
+                                -moz-transform: rotate(30deg);
+                                -o-transform: rotate(30deg);
+                                transform: rotate(30deg);
+                            }
+                        }
+                    }
+                }
+                dd {
+                    line-height: 22px;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    a {
+                        color: #555454;
+                    }
+                }
+                time {
+                    color: #066;
+                    margin-left: 10px;
+                }
             }
         }
     }
